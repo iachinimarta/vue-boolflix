c1a7ba27dd4286438c777ec6c917fb171cf32e51
Marta Iachini <iachinimarta@gmail.com>
2022-08-30 14:47:53 +0200

    Correzione errori

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <MyHeader/>
     <MyMain/>
   </div>
 </template>
 
 <script>
-import MyHeader from './components/MyHeader.vue';
 import MyMain from './components/MyMain.vue';
 
 export default {
   name: 'App',
   components: {
-    MyHeader,
     MyMain
   }
 }
